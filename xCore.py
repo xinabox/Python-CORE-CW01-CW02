e717730d7698db1a7268e8388fbd655c8920d45d
luqmaanb <lb@xinabox.cc>
2020-05-28 15:35:53 +0200

    Update

--- xCore.py
+++ xCore.py
@@ -10,8 +10,8 @@ class xCore:
         else:
             scl=22
             sda=21
-     
-        self.i2c = I2C(scl=Pin(scl), sda=Pin(sda), freq=100000)   
+
+        self.i2c = I2C(scl=Pin(scl), sda=Pin(sda), freq=100000)
 
     def write_read(self, addr, reg, length, repeat=False):
         self.i2c.writeto(addr, bytearray([reg]), repeat)
@@ -28,6 +28,17 @@ class xCore:
         raw = self.i2c.readfrom(addr, length)
         return raw
 
+    def writev(self, addr, vec, repeat=True):
+        self.i2c.writevto(addr, vec, repeat)
+
     def sleep(time):
         sleep_ms(time)
 
+    def stop(self):
+        self.i2c.stop()
+
+    def start(self):
+        self.i2c.start()
+
+    def write_(self, val):
+        self.i2c.write(val)
